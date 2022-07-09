d858badcd696f30d312d60d8c24f43cf07265bfa
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2022-07-09 00:23:17 -0400

    SuperMax works

--- main.py
+++ main.py
@@ -1,70 +1,45 @@
-import requests
-from bs4 import BeautifulSoup as bs
-import re 
-import selenium 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-from selenium.webdriver.chrome.options import Options
-import os
-import resource
 
-resource.setrlimit (resource.RLIMIT_DATA,
-                    (resource.RLIM_INFINITY
-                    ,resource.RLIM_INFINITY)) 
+def SuperMax(): 
+    #This variable takes the input of the item you want to search
+    producto = input("Entre el nombre del producto: ")
+    #Driver that makes all the magic happen (Chrome v.103)
+    driver = webdriver.Chrome(executable_path="chromedriver")
+    #Link of the website
+    driver.get("https://www.supermaxonline.com/shopping-home.html?utm_campaign=hub_smax_pri_online_srch_traf_pros&utm_source=google&utm_medium=paid-search&utm_content=online_clientes-nuevos-1_text-2&gclid=EAIaIQobChMI2JuQmrLq-AIVdPHjBx18OgEvEAAYASAAEgLD5_D_BwE")
 
-producto = input("Entre el nombre del producto: ")
+    #Takes the product name and uses the search bar for getting data
+    driver.find_element(By.ID, 'header-search').send_keys(producto)
+    search = driver.find_element(By.CLASS_NAME, 'btn.btn-default')
+    search.click()
+    #Ad break 
+    time.sleep(10)
 
-driver = webdriver.Chrome(executable_path="chromedriver.exe")
+    #Makes the element a variable for easier approach
+    boton_load_more = driver.find_element(By.ID, 'btn-load-more')
+    #While loop for load more buttons 
+    while True: 
+        if boton_load_more.is_displayed() == True:
+            load = driver.find_element(By.ID, 'btn-load-more')    
+            load.click()
+            #Increase if error is given, internet may be slow 
+            time.sleep(1)
+            boton_load_more = driver.find_element(By.ID, 'btn-load-more')
+        #If no buttons are displayed, gets out of loop
+        else: 
+            break 
+    #Checks if prodcut name is in element and creates a string contaning the items names, weights, and prices
+    if producto.casefold or producto.capitalize in driver.find_element(By.CSS_SELECTOR, producto):
+        resultados = driver.find_element(By.ID, "products").text
+    #Writes text file with the data and names it the same as the product name you used as input
+    with open(producto + ".txt", "w") as text_file:
+        text_file.write(resultados)
 
-driver.get("https://www.supermaxonline.com/shopping-home.html?utm_campaign=hub_smax_pri_online_srch_traf_pros&utm_source=google&utm_medium=paid-search&utm_content=online_clientes-nuevos-1_text-2&gclid=EAIaIQobChMI2JuQmrLq-AIVdPHjBx18OgEvEAAYASAAEgLD5_D_BwE")
+    #close driver after 180 seconds aka closes chrome
+    time.sleep(180)
+    driver.close()
 
-#input_box = driver.find_elements(By.CLASS_NAME, 'form-inline')
-#print(len(input_box))
-
-driver.find_element(By.ID, 'header-search').send_keys(producto)
-search = driver.find_element(By.CLASS_NAME, 'btn.btn-default')
-search.click()
-
-time.sleep(10)
-
-#i = 1
-
-#while i < 10: 
-    #if producto.casefold or producto.capitalize in driver.find_element(By.CSS_SELECTOR, producto):
-      #  resultados = driver.find_element(By.CLASS_NAME, "product-space").text
-      #  print(resultados)
-
-boton_load_more = driver.find_element(By.ID, 'btn-load-more')
-
-print(driver.find_element(By.ID, 'btn-load-more'))
-
-while True: 
-    if boton_load_more.is_displayed() == True:
-        load = driver.find_element(By.ID, 'btn-load-more')    
-        load.click()
-        time.sleep(0.5)
-        boton_load_more = driver.find_element(By.ID, 'btn-load-more')
-    else: 
-        break 
-
-if producto.casefold or producto.capitalize in driver.find_element(By.CSS_SELECTOR, producto):
-    resultados = driver.find_element(By.ID, "products").text
-    #[x.strip().split('--------------') for x in re.findall('((?:[^\n]+\n?){1,3})', producto)]
-    print(resultados)
-    #precio = driver.find_element(By.CLASS_NAME, "text-center precio")
-    #print(precio.get_attribute('style'))
-
-#driver.find_element(By.CSS_SELECTOR, "[title*=" + producto + "]")
-#print(driver.find_element(By.CLASS_NAME, "text-center precio")).text
-
-time.sleep(60)
-driver.close()
-
-#headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.49" }
-
-#load HTML code 
-#page = requests.get("https://www.supermaxonline.com/shopping-home.html?utm_campaign=hub_smax_pri_online_srch_traf_pros&utm_source=google&utm_medium=paid-search&utm_content=online_clientes-nuevos-1_text-2&gclid=EAIaIQobChMI2JuQmrLq-AIVdPHjBx18OgEvEAAYASAAEgLD5_D_BwE", headers = headers)
-
-
-#print(page.text)
+if __name__ == "__main__": 
+    SuperMax()
