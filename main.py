9ea35ccfb42b69ecb58abed0eb899c889992dfb3
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2022-07-09 02:36:07 -0400

    Headless Chrome

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.chrome.options import Options
+
 
 def SuperMax(): 
-    #This variable takes the input of the item you want to search
+    #Makes the chrome run headless
+    #Driver that makes all the magic happen (Chrome v.103)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.headless = True
+
+    driver = webdriver.Chrome(
+            executable_path="chromedriver", chrome_options=chrome_options)
+   #This variable takes the input of the item you want to search
     producto = input("Entre el nombre del producto: ")
     #Driver that makes all the magic happen (Chrome v.103)
-    driver = webdriver.Chrome(executable_path="chromedriver")
+                                                                    #driver = webdriver.Chrome(executable_path="chromedriver")
     #Link of the website
     driver.get("https://www.supermaxonline.com/shopping-home.html?utm_campaign=hub_smax_pri_online_srch_traf_pros&utm_source=google&utm_medium=paid-search&utm_content=online_clientes-nuevos-1_text-2&gclid=EAIaIQobChMI2JuQmrLq-AIVdPHjBx18OgEvEAAYASAAEgLD5_D_BwE")
 
@@ -36,10 +45,15 @@ def SuperMax():
     #Writes text file with the data and names it the same as the product name you used as input
     with open(producto + ".txt", "w") as text_file:
         text_file.write(resultados)
+    print("*********************************************")
+    print("You can close the program, the file is saved.")
 
     #close driver after 180 seconds aka closes chrome
     time.sleep(180)
     driver.close()
 
 if __name__ == "__main__": 
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    driver = webdriver.Chrome("chromedriver", options=options)
     SuperMax()
